9401cf63a24c2810e23d7322beecdac6bb16b94c
gozalbesnavarro@gmail.com <gozalbesnavarro@gmail.com>
2024-09-19 16:17:56 +0200

    Planetario Modulo 4

---
+++ Assets/Scripts/Venus.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Venus : MonoBehaviour
+{
+    // Public Attributes
+    public float speed;
+    public float speedRotation;
+    public Transform sun;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.up, speedRotation * Time.deltaTime);
+        transform.RotateAround(sun.position, transform.up, speed * Time.deltaTime);
+        Debug.DrawRay(transform.position, transform.forward * 2, Color.blue);
+    }
+}
